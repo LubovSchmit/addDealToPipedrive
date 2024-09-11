981b0a64a645dc9629efac799122fe9819f2cee9
LubovSchmit <81571492+LubovSchmit@users.noreply.github.com>
2024-09-11 14:13:27 +0200

    refactor

--- pipedrive-deal/app.js
+++ pipedrive-deal/app.js
@@ -12,20 +12,8 @@ async function addDeal() {
 
         const api = new pipedrive.DealsApi(defaultClient);
 
-        // Данные для создания сделки
         const data = {
             title: 'Deal of the century',
-            value: '10000', // Значение должно быть строкой
-            currency: 'USD',
-            user_id: null, // Использовать пользователя, делающего запрос
-            person_id: null, // Не указан, нужно либо указать person_id, либо org_id
-            org_id: 1,
-            stage_id: 1, // Убедитесь, что этот stage_id корректен
-            status: 'open',
-            expected_close_date: '2024-12-31', // Пример даты в формате YYYY-MM-DD
-            probability: 60,
-            lost_reason: null,
-            visible_to: '3' // Пример значения, зависящего от видимости
         };
 
         const response = await api.addDeal(data);
